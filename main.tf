9156358a4f554781fe4abb14558eb01e7b369d23
Joe Colandro <jcolandro@macbookjoe.lan>
2020-06-24 14:30:41 -0400

    cluster init

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "jdc-sandbox"
 
     workspaces {
-      name = "learn-terraform-pipelines-vault"
+      name = "vault-gke"
     }
   }
 }
